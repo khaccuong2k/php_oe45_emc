0957dd1318a672a3f23753f7bfec2e053f78d9ae
khaccuong2k <khac.uong2k@gmail.com>
2021-06-28 08:39:25 +0700

    implement i18n for some pages, will done in pull after

---
+++ resources/lang/en/lable.php
@@ -0,0 +1,100 @@
+<?php
+
+return [
+    'title' => [
+        'user' => [
+            'index' => 'Manage Customers',
+            'detail' => 'Detail Customer',
+        ],
+        'dashboard' => [
+            'index' => 'Dashboard',
+        ],
+        'order' => [
+            'index' => 'Manage Orders',
+            'detail' => 'Detail Order',
+        ],
+        'product' => [
+            'index' => 'Manage Products',
+            'add' => 'Add Product',
+            'edit' => 'Edit Product',
+            'detail' => 'Detail Product',
+        ],
+        'request' => [
+            'index' => 'Manage Requests',
+            'detail' => 'Detail Request',
+        ],
+        'category' => [
+            'index' => 'Manage Categories',
+            'add' => 'Add Category',
+            'edit' => 'Edit Category',
+            'detail' => 'Detail Category',
+        ],
+    ],
+    'action' => 'Action',
+    'action_view' => 'Detail',
+    'action_add' => 'Add',
+    'action_edit' => 'Edit',
+    'action_delete' => 'Delete',
+    'app' => [
+        'loading' => 'Loading...',
+        'search' => [
+            'search' => 'Search',
+            'from' => 'From',
+            'to' => 'To',
+            'subject' => 'Subject',
+        ],
+        'user' => [
+            'profile' => 'Profile',
+            'setting' => 'Setting',
+            'help' => 'Help',
+            'logour' => 'Log out',
+        ],
+        'setting' => [
+            'layout_setting' => 'Layout Settings',
+            'user_interface_settings' => 'User Interface Settings',
+            'header_background' => 'Header Background',
+            'sidebar_background' => 'Sidebar Background',
+            'menu_dropdown_icon' => 'Menu Dropdown Icon',
+            'menu_list_icon' => 'Menu List Icon',
+            'reset_settings' => 'Reset Settings',
+            'white' => 'White',
+            'dark' => 'Dark', 
+        ],
+        'sidebar' => [
+            'list' => 'List',
+            'add' => 'Add',
+            'chat' => 'Chat',
+        ],
+        'home' => 'Home',
+    ],
+    'user' => [
+        'list' => 'List User',
+        'name' => 'Name',
+        'avatar' => 'Avatar',
+        'total_bought' => 'Total Bought',
+        'detail' => 'Detail User',
+        'address' => 'Address',
+        'phone' => 'Phone',
+        'email' => 'Email',
+        'birthday' => 'Birthday',
+    ],
+    'category' => [
+        'list' => 'List Category',
+        'name' => 'Name',
+        'thumbnail' => 'Thumbnail',
+        'description' => 'Description',
+        'parent_id' => 'Parent Category',
+        'detail' => 'Detail Category',
+    ],
+    'order' => [
+        'name' => 'Name Products',
+        'thumbnail' => 'Thumbnail',
+        'qty' => 'Quantity',
+        'user_order' => 'Customer Order',
+        'type_payment' => 'Type Payment',
+        'status' => 'Status',
+    ],
+    'product' => [
+        'detail' => 'View Detail',
+    ],
+];
--- resources/views/admin/product/detail.blade.php
+++ resources/views/admin/product/detail.blade.php
@@ -1,101 +1,75 @@
 @extends('admin.layouts.app')
 
 @section('title')
-    <title>Detail Product</title>
+<title>Detail Product</title>
 @endsection
 
 @section('css')
-    <!-- Site favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('admin-page/vendors/images/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('admin-page/vendors/images/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('admin-page/vendors/images/favicon-16x16.png') }}">
-
-    <!-- Mobile Specific Metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-    <!-- Google Font -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-page/src/fonts/google-font/inter.css') }}">
-    <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-page/vendors/styles/core.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-page/vendors/styles/icon-font.min.css') }}">
-    <!-- Slick Slider css -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-page/src/plugins/slick/slick.css') }}">
-    <!-- bootstrap-touchspin css -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-page/src/plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin-page/vendors/styles/style.css') }}">
+<!-- Site favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('admin-page/vendors/images/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('admin-page/vendors/images/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('admin-page/vendors/images/favicon-16x16.png') }}">
+<!-- Mobile Specific Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!-- Google Font -->
+<link rel="stylesheet" type="text/css" href="{{ asset('admin-page/src/fonts/google-font/inter.css') }}">
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="{{ asset('admin-page/vendors/styles/core.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('admin-page/vendors/styles/icon-font.min.css') }}">
+<!-- Slick Slider css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('admin-page/src/plugins/slick/slick.css') }}">
+<!-- bootstrap-touchspin css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('admin-page/src/plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('admin-page/vendors/styles/style.css') }}">
 @endsection
 
 @section('content')
-    <div class="main-container">
-        <div class="pd-ltr-20 xs-pd-20-10">
-            <div class="min-height-200px">
-                <div class="page-header">
-                    <div class="row">
-                        <div class="col-md-12 col-sm-12">
-                            <div class="title">
-                                <h4>Product Detail</h4>
-                            </div>
-                            <nav aria-label="breadcrumb" role="navigation">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Product Detail</li>
-                                </ol>
-                            </nav>
-                        </div>
+<div class="product-wrap">
+    <div class="product-detail-wrap mb-30">
+        <div class="row">
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="product-slider slider-arrow">
+                    <div class="product-slide">
+                        <img src="{{ asset('admin-page/vendors/images/product-img1.jpg') }}" alt="">
+                    </div>
+                    <div class="product-slide">
+                        <img src="{{ asset('admin-page/vendors/images/product-img2.jpg') }}" alt="">
+                    </div>
+                    <div class="product-slide">
+                        <img src="{{ asset('admin-page/vendors/images/product-img3.jpg') }}" alt="">
+                    </div>
+                    <div class="product-slide">
+                        <img src="{{ asset('admin-page/vendors/images/product-img4.jpg') }}" alt="">
                     </div>
                 </div>
-                <div class="product-wrap">
-                    <div class="product-detail-wrap mb-30">
-                        <div class="row">
-                            <div class="col-lg-6 col-md-12 col-sm-12">
-                                <div class="product-slider slider-arrow">
-                                    <div class="product-slide">
-                                        <img src="{{ asset('admin-page/vendors/images/product-img1.jpg') }}" alt="">
-                                    </div>
-                                    <div class="product-slide">
-                                        <img src="{{ asset('admin-page/vendors/images/product-img2.jpg') }}" alt="">
-                                    </div>
-                                    <div class="product-slide">
-                                        <img src="{{ asset('admin-page/vendors/images/product-img3.jpg') }}" alt="">
-                                    </div>
-                                    <div class="product-slide">
-                                        <img src="{{ asset('admin-page/vendors/images/product-img4.jpg') }}" alt="">
-                                    </div>
-                                </div>
-                                <div class="product-slider-nav">
-                                    <div class="product-slide-nav">
-                                        <img src="{{ asset('admin-page/vendors/images/product-img1.jpg') }}" alt="">
-                                    </div>
-                                    <div class="product-slide-nav">
-                                        <img src="{{ asset('admin-page/vendors/images/product-img2.jpg') }}" alt="">
-                                    </div>
-                                    <div class="product-slide-nav">
-                                        <img src="{{ asset('admin-page/vendors/images/product-img3.jpg') }}" alt="">
-                                    </div>
-                                    <div class="product-slide-nav">
-                                        <img src="{{ asset('admin-page/vendors/images/product-img4.jpg') }}" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-12 col-sm-12">
-                                <div class="product-detail-desc pd-20 card-box height-100-p">
-                                    <h4 class="mb-20 pt-20">Gufram Bounce Black</h4>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                    <div class="price">
-                                        <del>$55.5</del><ins>$49.5</ins>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div class="product-slider-nav">
+                    <div class="product-slide-nav">
+                        <img src="{{ asset('admin-page/vendors/images/product-img1.jpg') }}" alt="">
+                    </div>
+                    <div class="product-slide-nav">
+                        <img src="{{ asset('admin-page/vendors/images/product-img2.jpg') }}" alt="">
+                    </div>
+                    <div class="product-slide-nav">
+                        <img src="{{ asset('admin-page/vendors/images/product-img3.jpg') }}" alt="">
+                    </div>
+                    <div class="product-slide-nav">
+                        <img src="{{ asset('admin-page/vendors/images/product-img4.jpg') }}" alt="">
                     </div>
                 </div>
             </div>
-            <div class="footer-wrap pd-20 mb-20 card-box">
-                DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a>
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="product-detail-desc pd-20 card-box height-100-p">
+                    <h4 class="mb-20 pt-20">Gufram Bounce Black</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <div class="price">
+                        <del>$55.5</del><ins>$49.5</ins>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('script')
